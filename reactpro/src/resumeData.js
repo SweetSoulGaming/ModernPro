85d5c4e723172c3c8d8ddeeb8c83a996e2caa4ea
Mike Thomas <mthomas.theadvocate@gmail.com>
2021-09-27 15:55:13 -0700

    React Key components

---
+++ reactpro/src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+  "imagebaseurl":"https://sweetsoulgaming.github.io/",
+  "name": "Michael Thomas",
+  "role": "Frontend Developer and Content Creator",
+  "linkedinId":" Michael Thomas",
+  "zoomid": "Rhyno",
+  "roleDescription": "First I'm passionate about gaming, and my goal is to get my foot in the door in the gaming industry. I'm also Journalism major making a career changing decision. I've found my joys and anixety of learning to code. It something that keeps bring you back or it'll be a skill you'd regret not honing and practicing. ",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/michael-thomas-ab0845213/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/SweetSoulGaming",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"https://twitter.com/SweetSoulBroth3",
+        "className":"fa fa-twitter"
+      }
+    ],
+  "aboutme":"I am currently in a bootcamp at UC Riverside Extention and about to complete.",
+  "address":"India",
+  "website":"https://sweetsoulgaming.github.io/ModernPro/",
+  "education":[
+    {
+      "UniversityName":"UC Riverside Extention",
+      "specialization":"Writing",
+      "MonthOfPassing":"Oct",
+      "YearOfPassing":"2021",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "UniversityName":"Contra Costa College",
+      "specialization":"Journalism",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2019",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Amazon",
+      "specialization":"Prime Shopper",
+      "MonthOfLeaving":"",
+      "YearOfLeaving":"2019",
+      "Achievements":"MTR"
+    },
+    {
+      "CompanyName":"",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"FoodiePals",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",  
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
